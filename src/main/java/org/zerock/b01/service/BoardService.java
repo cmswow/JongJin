b8b76644936d2113f774e0f4fe340081d3c993d3
cmswow <cmswow28@gmail.com>
2023-08-01 17:53:16 +0900

    20230801

--- src/main/java/org/zerock/b01/service/BoardService.java
+++ src/main/java/org/zerock/b01/service/BoardService.java
@@ -1,6 +1,8 @@
 package org.zerock.b01.service;
 
 import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 
 public interface BoardService {
 
@@ -10,5 +12,7 @@ public interface BoardService {
 
     void modify(BoardDTO boardDTO);
 
+    void remove(Long bno);
 
+    PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
 }
--- src/test/java/org/zerock/b01/service/BoardServiceImplTests.java
+++ src/test/java/org/zerock/b01/service/BoardServiceImplTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.b01.domain.Board;
 import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 
 @SpringBootTest
 @Log4j2
@@ -49,5 +51,26 @@ class BoardServiceImplTests {
                 .content("수정 내용 테스트")
                 .build();
                 boardService.modify(boardDTO);
+    }
+
+    @Test
+    public void testRemove(){
+        boardService.remove(101L);
+    }
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO =
+                PageRequestDTO.builder()
+                        .type("tcw")
+                        .keyword("1")
+                        .page(1)
+                        .size(10)
+                        .build();
+
+
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+        log.info("responseDTO===>" + responseDTO);
     }
 }
